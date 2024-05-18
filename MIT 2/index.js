c6a992da2b0e8e612cae09cfa00642cbba1c31e1
Kaviya-mfedu <164199387+Kaviya-mfedu@users.noreply.github.com>
2024-05-18 11:32:40 +0530

    Add files via upload

---
+++ MIT 2/index.js
@@ -0,0 +1,9 @@
+function func(){
+    var name=document.getElementById("name").value;
+    var mail=document.getElementById("mail").value;
+    console.log(name);
+    console.log(mail);
+    
+}
+
+
